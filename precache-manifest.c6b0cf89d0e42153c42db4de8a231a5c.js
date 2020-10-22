e1d7185d174edc840ddf1a4992d52d6655ced89d
SpeedwayArtist <speedway.artist@gmail.com>
2020-10-22 12:12:06 +0300

    Updates

---
+++ precache-manifest.c6b0cf89d0e42153c42db4de8a231a5c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a7417f60e919bc80e3d7a531349c57",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "01952db0229b67d08ed4",
+    "url": "/social-network/static/css/3.bb17435c.chunk.css"
+  },
+  {
+    "revision": "64aef48e0a4f01ec3394",
+    "url": "/social-network/static/css/4.0c13164c.chunk.css"
+  },
+  {
+    "revision": "1249636aac5541d5399f",
+    "url": "/social-network/static/css/main.cb3115b9.chunk.css"
+  },
+  {
+    "revision": "401731f611d191c0ab52",
+    "url": "/social-network/static/js/2.e7a7aa26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.e7a7aa26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01952db0229b67d08ed4",
+    "url": "/social-network/static/js/3.e8d821c4.chunk.js"
+  },
+  {
+    "revision": "64aef48e0a4f01ec3394",
+    "url": "/social-network/static/js/4.ec3fbc8a.chunk.js"
+  },
+  {
+    "revision": "1249636aac5541d5399f",
+    "url": "/social-network/static/js/main.a821068b.chunk.js"
+  },
+  {
+    "revision": "862430dbfd6fd758c231",
+    "url": "/social-network/static/js/runtime-main.f93238da.js"
+  },
+  {
+    "revision": "fe24ae50007b9d45aefeb5594c020efb",
+    "url": "/social-network/static/media/loader.fe24ae50.gif"
+  },
+  {
+    "revision": "d17c2f24c0a44f7ab4104ffa32df3e24",
+    "url": "/social-network/static/media/user_avatar_placeholder.d17c2f24.svg"
+  }
+]);
